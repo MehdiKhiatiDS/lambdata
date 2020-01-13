075d6e94e0b19ed4f056c97ad051fd592ad4829b
Mehdi Khiati <mehdi1710@me.com>
2020-01-13 15:52:34 -0800

    added some proof of concept utility files

---
+++ lambdata_MehdiKhiati/__init__.py
@@ -0,0 +1,11 @@
+"""
+lambdata - a collection of data science helper functions
+"""
+
+
+import pandas as pd 
+import numpy as np 
+
+# sample code
+ONE =  pd.DataFrame(np.ones(10))
+Zeros = pd.DataFrame(np.zeros(50))
